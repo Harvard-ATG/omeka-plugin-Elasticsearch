56e89f16587496581c617365d41cbe711973b707
Arthur Barrett <abarrett@fas.harvard.edu>
2018-02-21 10:43:38 -0500

    Update setup.sh to use omeka 2.6

--- .travis/setup.sh
+++ .travis/setup.sh
@@ -13,9 +13,9 @@ plugin_dest=$OMEKA_CORE_DIR/plugins/$plugin_name
 mysql -e 'CREATE DATABASE omeka;' -uroot
 
 # Grab specified version of Omeka from github
-wget -nv -O /tmp/omeka-2.5.1.zip https://github.com/omeka/Omeka/releases/download/v2.5.1/omeka-2.5.1.zip
-unzip -q /tmp/omeka-2.5.1.zip -d /tmp
-mv -v /tmp/omeka-2.5.1 $OMEKA_CORE_DIR
+wget -nv -O /tmp/omeka-2.6.zip https://github.com/omeka/Omeka/releases/download/v2.6/omeka-2.6.zip
+unzip -q /tmp/omeka-2.6.zip -d /tmp
+mv -v /tmp/omeka-2.6 $OMEKA_CORE_DIR
 
 # Update Omeka db.ini
 sed -i 's/^host.*/host = "localhost"/' $OMEKA_CORE_DIR/db.ini
